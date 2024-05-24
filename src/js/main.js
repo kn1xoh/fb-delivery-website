65edd23da7bd38a9061a5d57c1354b1d2f3ac8d3
kn1xoh <khozin.sergei@gmail.com>
2024-05-24 14:24:31 +0300

    add slider swiper

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Инициализация слайдера
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
