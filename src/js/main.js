92978949cd4fecf38b7b3c48f65ba2c91290a435
kn1xoh <khozin.sergei@gmail.com>
2024-05-30 17:35:52 +0300

    add adaptive and burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,26 @@
 // Инициализация слайдера
 const swiper = new Swiper(".swiper", {
-  slidesPerView: "auto",
+  slidesPerView: 1,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    1251: {
+      slidesPerView: "auto",
+    },
+  },
+});
+
+// Бургер
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger-menu");
+const closeMenu = document.querySelector(".burger-menu__close");
+
+burger.addEventListener("click", function () {
+  burgerMenu.classList.add("burger-menu--active");
+});
+
+closeMenu.addEventListener("click", function () {
+  burgerMenu.classList.remove("burger-menu--active");
 });
